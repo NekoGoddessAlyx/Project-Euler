e89a5cddd3a32b0410e7b189789c5b10d18ae6b2
Alyxandria Rose <NekoGoddessAlyx@users.noreply.github.com>
2022-07-21 16:45:06 -0700

    Problem 21

---
+++ kotlin/src/main/kotlin/Solution0021.kt
@@ -0,0 +1,15 @@
+class Solution0021 : Solution() {
+
+    override fun solve(): String {
+        // [n : d(n)]
+        val sumOfProperDivisors = (1L until 10_000L)
+            .associateWith { n -> getDivisors(n).filter { d -> d < n }.sum() }
+
+        return sumOfProperDivisors
+            .filter { it.key != it.value && sumOfProperDivisors.getOrDefault(it.value, 0) == it.key }
+            .keys
+            .sum()
+            .toString()
+    }
+
+}
