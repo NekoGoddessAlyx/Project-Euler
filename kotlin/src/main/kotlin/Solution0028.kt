5a32bbc92e0e4facbd2c924a7275366240d27fb4
Alyxandria Rose <NekoGoddessAlyx@users.noreply.github.com>
2022-07-27 20:59:49 -0700

    Problem 28 but closed form function yay!

--- kotlin/src/main/kotlin/Solution0028.kt
+++ kotlin/src/main/kotlin/Solution0028.kt
@@ -1,7 +1,15 @@
 class Solution0028 : Solution() {
 
     override fun solve(): String {
-        return (1 + (3..1001).step(2).sumOf { (it - 2) * (it - 2) * 4 + (it - 1) * 10 }).toString()
+//        return (1 + (3..1001).step(2).sumOf { (it - 2) * (it - 2) * 4 + (it - 1) * 10 }).toString()
+
+        // hhhhhhhh
+        val n = 1001.0
+        val s = 2.0 * n * n * n / 3.0 +
+                n * n / 2.0 +
+                4.0 * n / 3.0 +
+                -1.5
+        return s.toLong().toString()
     }
 
 }
