572d2b298a0f78776c863c84a90e5e9537198297
Alyxandria Rose <NekoGoddessAlyx@users.noreply.github.com>
2022-07-31 20:11:02 -0700

    Should be able to add new files without needing to touch the main file

--- project_euler_rust/src/main.rs
+++ project_euler_rust/src/main.rs
@@ -1,3 +1,35 @@
+#![feature(local_key_cell_methods)]
+
+use std::cell::RefCell;
+
+struct Problem {
+    problem_number: u32,
+    solver: fn() -> String,
+}
+
+fn register(problem_number: u32, solver: fn() -> String) {
+    SOLVERS.with_borrow_mut(|s| {
+        let index = if problem_number as usize > s.len() { s.len() } else { problem_number as usize };
+        s.insert(index, Problem { problem_number, solver });
+    });
+}
+
+thread_local! {
+    static SOLVERS: RefCell<Vec<Problem>> = RefCell::new(Vec::new());
+}
+
 fn main() {
-    println!("Hello, world!");
+    SOLVERS.with(|s| {
+        let s = s.borrow();
+        println!("Project Euler (rust edition)");
+        println!("{} problem(s) have been solved.", s.len());
+        // println!();
+
+        let iter = s.iter();
+        for problem in iter {
+            let num = problem.problem_number;
+            let solver = problem.solver;
+            println!("Problem {} (https://projecteuler.net/problem={}): {}", num, num, solver())
+        }
+    });
 }
